7cbdcd5d76e73629e8911e2d3bd4ba3466ebea48
Ricardo <ricardodacruz1@hotmail.com>
2023-09-05 17:33:49 -0300

    botoes

--- src/cartaoProduto.js
+++ src/cartaoProduto.js
@@ -12,7 +12,7 @@ for (const produtoCatalogo  of catalogo) {
     <p class='text=sm'>${produtoCatalogo.marca}</p>
     <p class='text-sm'>${produtoCatalogo.nome}</p>
     <p class='text-sm'>$${produtoCatalogo.preco}</p>
-      <button id=adicionar-${produtoCatalogo.id} 'class='bg-slate-950 hover:bg-slate-400 text-slate-200'
+      <button class='bg-slate-950 hover:bg-slate-700 text-slate-200'
       ><i class="fa-solid fa-cart-plus" ></i></button>
     </div>`;
     document.getElementById("container-produto").innerHTML += cartaoProduto;
