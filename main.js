30fa1ad1cce09ee3b57a588213331934111a5b66
Ricardo <ricardodacruz1@hotmail.com>
2023-10-02 20:33:23 -0300

    pagina

--- main.js
+++ main.js
@@ -5,6 +5,7 @@
 // const nomeArquivoImagem = "product-1.jpg";
 
 import { renderizarCatalogo } from "./src/cartaoProduto";
+import { inicializarFiltros } from "./src/fitrosCatalogos";
 import { inicializarCarrinho,atualizarPrecoCarrinho, renderizarProdutosCarrinho } from "./src/menuCarrinho";
 
 //import { renderizarProdutosCarrinho } from "./src/menuCarrinho";    
@@ -15,3 +16,5 @@ renderizarCatalogo();
 inicializarCarrinho();
 renderizarProdutosCarrinho();
 atualizarPrecoCarrinho();
+inicializarFiltros();
+inicializarFiltros;
